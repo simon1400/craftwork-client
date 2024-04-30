5f716488abf626a82bb1acd185c8b261aa871610
Your Name <>
2024-04-30 22:23:29 +0200

    add gtag

--- layout/Page/index.tsx
+++ layout/Page/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import { selectAllMeta } from "stores/slices/metaSlices";
+import Script from "next/script";
 
 interface IPage {
   children: ReactNode | ReactNode[];
@@ -110,14 +111,14 @@ const Page: FC<IPage> = ({ children, className = "", id = "" }) => {
       </Head>
 
       {/* <!-- Google tag (gtag.js) --> */}
-      {/* <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2CMP76GFG2" />
-      <Script id="gtag" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-2CMP76GFG2');`}
-      </Script> */}
+      {/* <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2CMP76GFG2" /> */}
+      <Script strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-53S796KX');`}
+      </Script>
 
       {/* Hotjar Tracking Code for my site */}
       {/* <Script id="hotjar">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="cs">
       <Head />
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-53S796KX"
+        height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Main />
         <NextScript />
       </body>
